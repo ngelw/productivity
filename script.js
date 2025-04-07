e672ef72b385f3aee5f5638e0cc47e8436088c5b
Angel <angelshrestha2024@gmail.com>
2025-04-07 17:35:01 -0500

    prayers functionality(plays audio) added

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 let a = document.getElementById("clock");
 
-const time = new Date();
+
 function getTime(){
+    const time = new Date();
     let hours = time.getHours();
+    if(hours>12){
+        hours = hours - 12;
+    }
     let minutes = time.getMinutes();
-    a.innerHTML = `${hours}:${minutes}`;
+    let seconds = time.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+
+    a.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+getTime();
+setInterval(getTime, 1000);
+function togglePlayPause() {
+    const audioPlayer = document.getElementById("audio-player");
+    const playPauseButton = document.querySelector(".play-pause-button");
+
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        playPauseButton.textContent = "Pause";
+    } else {
+        audioPlayer.pause();
+        playPauseButton.textContent = "Play";
+    }
 }
